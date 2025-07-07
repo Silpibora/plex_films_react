11a73ea574325c02b11267dde9ce1d84a9e83df2
Silpibora <silpipriyabora@gmail.com>
2025-07-07 15:16:48 +0530

    Add files via upload

---
+++ basic-react-app/src/sections.jsx
@@ -0,0 +1,13 @@
+import Cardcollection from "./cardcollection";
+
+export default function Section(){
+let styles={height:"23rem",backgroundColor:"black"}
+
+    return (
+    <div className="section" style={styles}>
+     <Cardcollection/>
+    </div>
+
+    );
+
+}
